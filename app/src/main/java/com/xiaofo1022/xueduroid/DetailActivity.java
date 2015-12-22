e537d92129994f978af4fcfae6f060c9a687e32c
Kurt.Yu <kurt.yu@perficientgdc.com.cn>
2015-12-22 17:09:07 +0800

    Almost finished contribute oil.

--- app/src/main/java/com/xiaofo1022/xueduroid/DetailActivity.java
+++ app/src/main/java/com/xiaofo1022/xueduroid/DetailActivity.java
@@ -30,20 +30,23 @@ public class DetailActivity extends AppCompatActivity {
         answerTitle.setText("孕妇轻度饮酒可能会引起孩子哪些症状？");
 
         layoutMain = (LinearLayout)findViewById(R.id.detail_main);
-        layoutMain.addView(createInfoTextView("过动、无法专心，以及冲动、注意力持续时间短，行为与患有注意力缺陷障碍的儿童相似，症状参考彪子，咱俩看会儿电视就干别的去了。"));
-        layoutMain.addView(createContributorTextView("--- 来自:MathLotus乐队小刘的贡献"));
+        layoutMain.addView(createInfoView("过动、无法专心，以及冲动、注意力持续时间短，行为与患有注意力缺陷障碍的儿童相似，症状参考彪子，咱俩看会儿电视就干别的去了。", ""));
+        layoutMain.addView(createInfoView("就俩人！张学友！周慧敏！", "--- 来自:MathLotus乐队小刘的贡献"));
+        layoutMain.addView(createInfoView("港台四大丧逼，王杰为首", "--- 来自:来自共慈园的热心听众的补充"));
+        layoutMain.addView(createInfoView("如果有人说“老师我知道是他干的！”薛哥下课还得捎带手儿主持一下正义！", "--- 来自:SOSO神经女青年的补充"));
     }
 
-    private TextView createInfoTextView(String info) {
-        TextView textView = (TextView)LayoutInflater.from(this).inflate(R.layout.textview_info, null);
-        textView.setText(info);
-        return textView;
-    }
-
-    private TextView createContributorTextView(String info) {
-        TextView textView = (TextView)LayoutInflater.from(this).inflate(R.layout.textview_contributor, null);
-        textView.setText(info);
-        return textView;
+    private View createInfoView(String info, String contributor) {
+        View view = LayoutInflater.from(this).inflate(R.layout.item_answer_info, null);
+        TextView textInfo = (TextView)view.findViewById(R.id.tv_answer_info);
+        textInfo.setText(info);
+        TextView textContributor = (TextView)view.findViewById(R.id.tv_answer_contributor);
+        if (contributor == null || contributor.equals("")) {
+            textContributor.setVisibility(View.INVISIBLE);
+        } else {
+            textContributor.setText(contributor);
+        }
+        return view;
     }
 
     @Override
--- app/src/main/java/com/xiaofo1022/xueduroid/MainActivity.java
+++ app/src/main/java/com/xiaofo1022/xueduroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xiaofo1022.xueduroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, ContributeOilActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
     }
